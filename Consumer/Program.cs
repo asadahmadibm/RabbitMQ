c3b6732b4308a4bf575912a1f6d436d0368fe847
Asad Ahmadi <asad.ahmadi.bm@gmail.com>
2022-11-01 17:34:38 +0330

    Create Consumer

---
+++ Consumer/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+string message = "";
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+channel.QueueDeclare("product");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, eventArgs) =>
+
+{
+
+    var body = eventArgs.Body.ToArray();
+
+    message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+
+};
+
+channel.BasicConsume(queue: "product", autoAck: true, noLocal:false,exclusive:false,arguments:null,consumer: consumer);
+Console.ReadKey();
+
+
